e2604d8a41101f898fbd5d6d2b06d93959bc8b12
Adrian Polak <adrian.polak20@gmail.com>
2022-10-16 17:48:23 +0200

    feat: add prisma (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 import { SchoolsModule } from './schools/schools.module';
 
 @Module({
-  imports: [SchoolsModule],
+  imports: [PrismaModule, SchoolsModule],
 })
 export class AppModule {}
