cb9286a81a73bf8b30361bbc78dc0e9d01a6dbd5
Adrian Polak <adrian.polak20@gmail.com>
2022-10-16 14:16:54 +0200

    feat: change to fastify adapter (#3)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+
+import type { NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   await app.listen(3000);
 }
 bootstrap();
