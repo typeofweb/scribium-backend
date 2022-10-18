51ee38a37b2b6fc3cbc45842417866ec7ba7b1fc
adipol1359 <adrian.polak20@gmail.com>
2022-10-18 16:13:12 +0200

    refactor(prisma): change provide method

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,21 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { PrismaClient } from '@prisma/client';
+
+export const PRISMA_TOKEN = Symbol('PRISMA_TOKEN');
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    {
+      provide: PRISMA_TOKEN,
+      useFactory: async () => {
+        const client = new PrismaClient();
+
+        await client.$connect();
+        return client;
+      },
+    },
+  ],
+  exports: [PRISMA_TOKEN],
 })
 export class PrismaModule {}
